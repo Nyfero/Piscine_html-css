b9b0c48ae86836ab38ff45f08046e6c2271d3ac5
gsap <gaetan.sap@gmail.com>
2022-04-29 16:23:50 +0200

    ex04

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,29 @@
+var list = document.getElementsByClassName("ft_list")[0];
+
+function create_todo() {
+	console.log(document.cookie);
+
+	var a = prompt("Please enter a TO DO:");
+	
+	if (a)
+	{
+		let p = document.createElement('div');
+		p.appendChild(document.createTextNode(a))
+		p.onclick = function() {
+			p.remove();
+			document.cookie = encodeURIComponent(list.innerHTML);
+		}
+		list.appendChild(p);
+		document.cookie = encodeURIComponent(list.innerHTML);
+	}
+	else
+		create_todo()
+}
+
+list.innerHTML = decodeURIComponent(document.cookie);
+for (let i = 0; i < list.children.length; i++) {
+	list.children[i].onclick = function() {
+		list.children[i].remove();
+	document.cookie = encodeURIComponent(list.innerHTML);
+	}
+}
